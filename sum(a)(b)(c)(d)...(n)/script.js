f8c9c886ad6a6b263912eb33d2e2f4f0f07d0bae
Tejaswip4 <tejaswi.peddini@gmail.com>
2025-06-28 23:53:25 +0530

    adding sum of numbers

---
+++ sum(a)(b)(c)(d)...(n)/script.js
@@ -0,0 +1,13 @@
+// let sum = function(a){
+//     return function(b){
+//         if(b){
+//             return sum(a+b)
+//         }
+//         return a;
+//     }
+// }
+
+
+let sum = a  => b => b ? sum(a+b) : a;
+
+console.log(sum(1)(2)(3)(4)());
